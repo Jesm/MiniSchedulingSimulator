bd5a0c3100d8c7121469edc5fdf2c16459a1e4fb
Jonathan Szablevski <jszablevski@hotmail.com>
2015-05-11 00:19:26 -0300

    Implementacao de jobs IO-Bound

--- js/App.js
+++ js/App.js
@@ -1,30 +1,42 @@
 var App={
 
 	intervalRef:null,
+	changedIntervalDuration:false,
 
 	cfg:{
 		quantumTimeInterval:.2,
-		changedIntervalDuration:false,
 
-		jobMaxQuantumTime:5,
+		jobMaxQuantumTime:6,
+		maxJobsPerMinute:120,
 	},
 
 	JobGenerator:{
 
-		createJobQuantumInterval:3,
-		createJobCounter:0,
+		lastJobCreatedTime:+new Date(),
+		probabilityIOBound:.33, // Multiply by 100 to get the percentage
 
 		execute:function(){
-			if(!this.createJobCounter)
+			var jobCreationAvg=60000/App.cfg.maxJobsPerMinute, now=+new Date();
+			if(now-this.lastJobCreatedTime>jobCreationAvg){
 				this.createJob();
-			this.createJobCounter=(this.createJobCounter+1)%this.createJobQuantumInterval;
+				this.lastJobCreatedTime=now;
+			}
 		},
 
 		createJob:function(){
 			var job={
 				progress:0,
-				duration:Math.floor(Math.random()*App.cfg.jobMaxQuantumTime)+1
+				gotIOInteration:false
 			};
+
+			if(Math.random()<=this.probabilityIOBound){
+				job.type='io-bound';
+				job.duration=1;
+			}
+			else{
+				job.type='cpu-bound';
+				job.duration=Math.floor(Math.random()*App.cfg.jobMaxQuantumTime)+1;
+			}
 
 			App.ReadyJobQueue.add(job);
 		}
@@ -57,6 +69,12 @@ var App={
 		},
 
 		processCurrentJob:function(){
+			if(this.currentJob.type=='io-bound'&&!this.currentJob.gotIOInteration){
+				App.WaitingJobQueue.add(this.currentJob);
+				this.currentJob=null;
+				return;
+			}
+
 			this.currentJob.progress++;
 
 			if(this.currentJob.progress==this.currentJob.duration){
@@ -65,6 +83,42 @@ var App={
 			}
 		}
 
+
+	},
+
+	WaitingJobQueue:{
+
+		minWaitingQuantumTime:3,
+		maxWaitingQuantumTime:6,
+
+		queue:[],
+
+		currWaitingQuantumTime:0,
+		currWaitingQuantumTimeExecuted:0,
+
+		add:function(t){
+			this.queue.push(t);
+		},
+
+		execute:function(){
+			if(!this.queue.length)
+				return;
+
+			if(!this.currWaitingQuantumTimeExecuted){
+				var r=(this.maxWaitingQuantumTime-this.minWaitingQuantumTime)*Math.random();
+				this.currWaitingQuantumTime=Math.floor(r)+this.minWaitingQuantumTime;
+			}
+
+			this.currWaitingQuantumTimeExecuted++;
+
+			if(this.currWaitingQuantumTimeExecuted==this.currWaitingQuantumTime){
+				this.currWaitingQuantumTimeExecuted=0;
+
+				var job=this.queue.shift();
+				job.gotIOInteration=true;
+				App.ReadyJobQueue.add(job);
+			}
+		}
 
 	},
 
@@ -79,6 +133,7 @@ var App={
 
 		this.JobGenerator.execute();
 		this.Cpu.execute();
+		this.WaitingJobQueue.execute();
 
 		if(this.changedIntervalDuration){
 			this.changedIntervalDuration=false;
